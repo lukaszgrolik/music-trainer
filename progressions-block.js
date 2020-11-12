8af24ac3c13a5cfc551e8636f109240a70add5a3
Łukasz Grolik <lukasz@grolik.pl>
2020-11-12 23:34:26 +0100

    Add more progressions; show chord scales

--- progressions-block.js
+++ progressions-block.js
@@ -1,13 +1,43 @@
 (() => {
+    const visibleScales = [
+        'major', 'ionian',
+        'minor', 'aeolian',
+        'major pentatonic', 'minor pentatonic', 'major blues', 'minor blues',
+        'dorian', 'phrygian', 'lydian', 'mixolydian',
+        'harmonic minor', 'dorian #4', 'phrygian dominant',
+        'melodic minor', 'mixolydian b6', 'lydian dominant', 'altered',
+        'double harmonic major', 'hungarian minor',
+        'bebop',
+        'flamenco',
+    ];
 
     const ProgressionsBlockTemplate = () => {
-        const keys = ['A'];
+        const keys = ['A', 'C', 'D', 'E', 'G'];
         const progressions = [
             ['Im', 'IVm', 'Vm'],
             ['Im', 'IVm', 'bVI', 'V7'],
             ['I', 'IV', 'V'],
-            ['I', 'IVm', 'I'],
-            ['I', 'IV', 'IVm', 'I'],
+            ['I', 'IV', 'V'],
+            // minor plagal cadence
+            {name: 'minor plagal cadence', chords: ['I', 'IVm', 'I'], info: 'iv-I instead of IV-I'},
+            {name: 'minor plagal cadence', chords: ['I', 'IV', 'IVm', 'I'], info: 'iv-I instead of IV-I'},
+            ...[
+                // minor
+                ['i', 'bVII', 'bVI'],
+                ['bVI', 'i', 'bVII', 'i'],
+                ['i', 'bVII', 'bVI', 'bVII'],
+                ['i', 'bVI', 'bIII', 'bVII'],
+                {chords: ['i', 'bVI', 'iv', 'V'], name: '', info: 'V instead of v'},
+                {chords: ['i', 'bVI', 'iv', 'V7'], name: '', info: 'V7 instead of Vm7'},
+                {chords: ['i', 'bVII', 'bVI', 'V7'], name: 'andalusian cadence'},
+                ['i', 'bIII', 'iv', 'bVI'],
+                ['i', 'bIII', 'bVI', 'V7'],
+                ['iiø', 'V7', 'i'],
+                // harmonic minor
+                {chords: ['iiø', 'V', 'i'], name: '', info: 'neoclassical, medieval'},
+                ['i', 'V', 'bVI', 'iv'],
+                ['i', 'bVI', 'iiø', 'V'],
+            ],
         ];
 
         const style = {
@@ -18,23 +48,66 @@
 
         return `<div style="${styleString(style)}">
             ${
-                progressions.map(prog => {
-                    const chords = Tonal.Progression.fromRomanNumerals(keys[0], prog);
+                randomSamples(progressions, 3).map(prog => {
+                    const key = randomSample(keys);
+                    const getChords = prog => prog instanceof Array ? prog : prog.chords;
+                    const chords = getChords(prog);
+                    const transformChord = chord => {
+                        return chord
+                            .replace(/([i,v]+)$/, (_, g) => {
+                                return g + 'm';
+                            })
+                            .replace(/([i,v]+)/, (_, g) => {
+                                return g.toUpperCase();
+                            });
+                    };
+                    const tonalChords = Tonal.Progression.fromRomanNumerals(key, chords.map(transformChord));
+                    const sharedScales = intersection(
+                        ...tonalChords.map(chord => {
+                            return Tonal.Chord.chordScales(chord);
+                        })
+                    );
+                    const style = {
+                        border: '1px solid #ddd',
+                        padding: '.5em',
+                        'margin-bottom': '.5em',
+                    };
 
-                    return `<div>
+                    return `<div style="${styleString(style)}">
+                        <div>
+                            <div style="font-weight: bold">${key} - ${chords.join('-')} (${tonalChords.join('-')})</div>
+                            <div>${prog.name || 'unnamed'}${prog.info ? ` | ${prog.info}` : ''}</div>
+                            <div style="font-size: .8em">
+                                ${
+                                    sharedScales.filter(s => visibleScales.includes(s)).map(s => {
+                                        return `<span style="border-bottom: 1px solid #ccc;">${s}</span>`;
+                                    }).join(', ')
+                                }
+                            </div>
+                        </div>
+
                         <table>
                             <tr>
                                 ${
-                                    chords.map((chord, i) => {
-                                        return `<th>${prog[i]} - ${chord}</th>`;
+                                    tonalChords.map((chord, i) => {
+                                        return `<th>${chords[i]} - ${chord}</th>`;
                                     }).join('')
                                 }
                             </tr>
 
                             <tr>
                                 ${
-                                    chords.map((chord, i) => {
-                                        return `<td>piano chord, fretboard chord, matching scales</td>`;
+                                    tonalChords.map((chord, i) => {
+                                        return `<td>
+                                            <!--<div>piano chord, fretboard chord</div>-->
+                                            <div style="font-size: .8em">
+                                                ${
+                                                    Tonal.Chord.chordScales(chord).filter(s => visibleScales.includes(s)).map(s => {
+                                                        return `<span style="border-bottom: 1px solid #ccc;">${s}</span>`;
+                                                    }).join(', ')
+                                                }
+                                            </div>
+                                        </td>`;
                                     }).join('')
                                 }
                             </tr>
--- utils.js
+++ utils.js
@@ -1,10 +1,44 @@
 (() => {
+
+    function randomSample(arr) {
+        const i = Math.floor(Math.random() * arr.length);
+        return arr[i];
+    }
+    window.randomSample = randomSample;
+
+    function randomSamples(arr, n) {
+        const items = arr.slice();
+        const res = new Array(n).fill(undefined);
+
+        for (let i = 0; i < n; i++) {
+            if (items.length === 0) break;
+            const randomIndex = Math.floor(Math.random() * items.length);
+            res[i] = items.splice(randomIndex, 1)[0];
+        }
+
+        return res;
+    }
+    window.randomSamples = randomSamples;
+
+    function intersection(...arrays) {
+        const rest = arrays.slice(1);
+
+        return arrays[0].reduce((memo, val) => {
+            const includesVal = arr => arr.includes(val);
+            const ok = rest.every(includesVal);
+
+            if (ok) memo.push(val);
+
+            return memo;
+        }, []);
+    }
+    window.intersection = intersection;
+
     function styleString(style) {
         return Object.entries(style).map(([prop, val]) => {
             return `${prop}: ${style[prop]}`;
         }).join('; ');
     }
-
     window.styleString = styleString;
 
     function chroma(note) {
@@ -28,7 +62,6 @@
 
         return res;
     }
-
     window.chroma = chroma;
 
 })();
